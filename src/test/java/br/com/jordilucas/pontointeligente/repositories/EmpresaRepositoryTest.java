65d3bfebf5ff0151a9df6686d356cf702014ac93
Jordi Lucas <jordilucas3@gmail.com>
2017-11-29 22:35:39 -0300

    Script de test dos repositories

---
+++ src/test/java/br/com/jordilucas/pontointeligente/repositories/EmpresaRepositoryTest.java
@@ -0,0 +1,48 @@
+package br.com.jordilucas.pontointeligente.repositories;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.jordilucas.pontointeligente.api.entities.Empresa;
+import br.com.jordilucas.pontointeligente.api.repositories.EmpresaRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+@ActiveProfiles("test")
+public class EmpresaRepositoryTest {
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	private static final String CNPJ = "51463645000100";
+	
+	@Before
+	public void setUp()throws Exception{
+		
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de test");
+		empresa.setCnpj(CNPJ);
+		this.empresaRepository.save(empresa);
+		
+	}
+	
+	@After
+	public final void tearDown() {
+		this.empresaRepository.deleteAll();
+	}
+	
+	@Test
+	public void testBuscaPorCNPJ() {
+		Empresa empresa = this.empresaRepository.findByCnpj(CNPJ);
+		assertEquals(CNPJ, empresa.getCnpj());
+	}
+	
+}
