043498a95b535de7ace070037baaf52fcbc5cd04
Jordi Lucas <jordi@joyjet.com>
2018-01-10 15:52:33 -0300

    Adicionado annotation de service no servico de lancamento

--- src/main/java/br/com/jordilucas/pontointeligente/api/service/impl/LancamentoServiceImpl.java
+++ src/main/java/br/com/jordilucas/pontointeligente/api/service/impl/LancamentoServiceImpl.java
@@ -7,11 +7,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import br.com.jordilucas.pontointeligente.api.entities.Lancamento;
 import br.com.jordilucas.pontointeligente.api.repositories.LancamentoRepository;
 import br.com.jordilucas.pontointeligente.api.services.LancamentoService;
 
+@Service
 public class LancamentoServiceImpl implements LancamentoService{
 	
 	private static final Logger log = LoggerFactory.getLogger(LancamentoServiceImpl.class);
